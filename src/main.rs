804e381b3aac48125fef3234079b7d7eb701ab1f
jst-r <jstr271@gmail.com>
2024-12-20 15:32:13 +0100

    a calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::BufRead;
+
+mod parser;
+
+use parser::{parse, LoafParser, Rule};
+use pest::Parser;
+
+fn main() {}
+
+fn _repl() {
+    for line in std::io::stdin().lock().lines() {
+        let line = line.unwrap();
+        match parse(&line) {
+            Ok(expr) => {
+                println!("{:?}", expr);
+            }
+            Err(err) => println!("{:?}", err),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::{eval_expr, parse};
+
+    #[test]
+    fn test_eval_expr() {
+        let cases = [
+            ("1", 1),
+            ("1 + 1", 2),
+            ("1 + 2 * 3", 7),
+            ("4 / 2 + 2 * 3", 8),
+            ("(1 + 2) * 3", 9),
+            ("-1 + 2", 1),
+            ("(1 + 2) * -3", -9),
+        ];
+
+        for (input, expected) in cases {
+            let expr = parse(input).unwrap();
+            assert_eq!(eval_expr(expr), expected);
+        }
+    }
 }
---
+++ src/parser/mod.rs
@@ -0,0 +1,97 @@
+use pest::{iterators::Pairs, pratt_parser::PrattParser, Parser};
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "parser/grammar.pest"]
+pub struct LoafParser;
+
+#[derive(Debug)]
+pub enum Expr {
+    Integer(i32),
+    BinOp {
+        lhs: Box<Expr>,
+        op: BinOp,
+        rhs: Box<Expr>,
+    },
+    Prefix {
+        op: PrefixOp,
+        rhs: Box<Expr>,
+    },
+}
+
+#[derive(Debug)]
+pub enum BinOp {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
+
+#[derive(Debug)]
+pub enum PrefixOp {
+    Negate,
+}
+
+lazy_static::lazy_static! {
+    static ref PRATT_PARSER: PrattParser<Rule> = {
+        use pest::pratt_parser::{Assoc::*, Op};
+        use Rule::*;
+
+        PrattParser::new()
+            .op(Op::infix(add, Left) | Op::infix(subtract, Left))
+            .op(Op::infix(multiply, Left) | Op::infix(divide, Left))
+            .op(Op::prefix(negate))
+    };
+}
+
+pub fn parse(input: &str) -> Result<Expr, pest::error::Error<Rule>> {
+    let mut pairs = LoafParser::parse(Rule::equation, input)?;
+    Ok(parse_expr(pairs.next().unwrap().into_inner()))
+}
+
+pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
+    PRATT_PARSER
+        .map_primary(|primary| match primary.as_rule() {
+            Rule::integer => Expr::Integer(primary.as_str().parse().unwrap()),
+            Rule::expr => parse_expr(primary.into_inner()),
+            rule => unreachable!("Expected atom, found rule: {:?}", rule),
+        })
+        .map_prefix(|op, rhs| match op.as_rule() {
+            Rule::negate => Expr::Prefix {
+                op: PrefixOp::Negate,
+                rhs: Box::new(rhs),
+            },
+            rule => unreachable!("Expected prefix, found rule: {:?}", rule),
+        })
+        .map_infix(|lhs, op, rhs| {
+            let op = match op.as_rule() {
+                Rule::add => BinOp::Add,
+                Rule::subtract => BinOp::Subtract,
+                Rule::multiply => BinOp::Multiply,
+                Rule::divide => BinOp::Divide,
+                rule => unreachable!("Expected bin_op, found rule: {:?}", rule),
+            };
+
+            Expr::BinOp {
+                lhs: Box::new(lhs),
+                op,
+                rhs: Box::new(rhs),
+            }
+        })
+        .parse(pairs)
+}
+
+pub fn eval_expr(expr: Expr) -> i32 {
+    match expr {
+        Expr::Integer(i) => i,
+        Expr::BinOp { lhs, op, rhs } => match op {
+            BinOp::Add => eval_expr(*lhs) + eval_expr(*rhs),
+            BinOp::Subtract => eval_expr(*lhs) - eval_expr(*rhs),
+            BinOp::Multiply => eval_expr(*lhs) * eval_expr(*rhs),
+            BinOp::Divide => eval_expr(*lhs) / eval_expr(*rhs),
+        },
+        Expr::Prefix { op, rhs } => match op {
+            PrefixOp::Negate => -eval_expr(*rhs),
+        },
+    }
+}
